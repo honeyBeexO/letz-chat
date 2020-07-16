325d02a4a6c5943cf9973c6f2183488b6fe58cb6
nouh ben kadi <benkadi.nouh@icloud.com>
2020-07-16 13:08:00 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chatx/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
